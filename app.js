9db13eae04be27aed93aff61b8dd565e68f3299a
Tim Kozemzak <timkozemzak@gmail.com>
2021-09-14 18:45:51 -0700

    config deps, l1

---
+++ app.js
@@ -0,0 +1,7 @@
+function add(n1, n2) {
+    return n1 + n2;
+}
+var number1 = 123;
+var number2 = 2.8;
+var results = add(number1, number2);
+console.log('results', results);
--- app.ts
+++ app.ts
@@ -1,1 +1,9 @@
-console.log('Your code goes here...');
+function add(n1: number, n2: number) {
+	return n1 + n2;
+}
+
+const number1 = 123;
+const number2 = 2.8;
+
+const results = add(number1, number2);
+console.log('results', results);
