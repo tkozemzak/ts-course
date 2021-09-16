bc0c3c4a7f7c88038908a871f3766df3597492ad
Tim Kozemzak <timkozemzak@gmail.com>
2021-09-16 16:59:10 -0700

    l2

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-function add(n1, n2) {
-    return n1 + n2;
-}
-var number1 = 123;
-var number2 = 2.8;
-var results = add(number1, number2);
-console.log('results', results);
+var person = {
+    name: 'Timothy',
+    age: 26,
+    hobbies: ['hiking', 'cars'],
+    role: [2, 'Software Engineer']
+};
+console.log(person);
--- app.ts
+++ app.ts
@@ -1,9 +1,13 @@
-function add(n1: number, n2: number) {
-	return n1 + n2;
-}
+const person: {
+	name: string;
+	age: number;
+	hobbies: string[];
+	role: [number, string];
+} = {
+	name: 'Timothy',
+	age: 26,
+	hobbies: ['hiking', 'cars'],
+	role: [2, 'Software Engineer'],
+};
 
-const number1 = 123;
-const number2 = 2.8;
-
-const results = add(number1, number2);
-console.log('results', results);
+console.log(person);
